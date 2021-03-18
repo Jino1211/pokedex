d79f4bdec7e8052a3abd9067c9d2410cae7e0225
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-18 15:28:21 +0200

    added GET endpoint
    for pokemon by name

--- server/routes/pokemon.js
+++ server/routes/pokemon.js
@@ -1,10 +1,29 @@
 const { Router } = require("express");
-
+const { axios, POKEAPI_BASE_URL } = require("../utils/pokeAPI");
 const pokemon = Router();
 
 //get pokémon details by name
 pokemon.get("/:name", (req, res) => {
-  res.send("Pokemon route");
+  const { name } = req.params;
+  axios
+    .get(`${POKEAPI_BASE_URL}/pokemon/${name}`)
+    .then((pokemon) => {
+      const pokemonData = {
+        name: pokemon.data.name,
+        id: pokemon.data.id,
+        height: pokemon.data.height,
+        weight: pokemon.data.weight,
+        types: pokemon.data.types,
+        sprites: {
+          back_default: pokemon.data.sprites.back_default,
+          front_default: pokemon.data.sprites.front_default,
+        },
+      };
+      res.send(pokemonData);
+    })
+    .catch((e) => {
+      console.log(e);
+    });
 });
 
 module.exports = pokemon;
--- server/utils/pokeAPI.js
+++ server/utils/pokeAPI.js
@@ -2,3 +2,4 @@ const { default: axios } = require("axios");
 const POKEAPI_BASE_URL = "https://pokeapi.co/api/v2";
 
 // You can use this file to write helper functions
+module.exports = { axios, POKEAPI_BASE_URL };

b5a7d33530cec510659b615ff19920d35d929edf
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-18 19:11:29 +0200

    added SearchArea and PokemonDetails
    added get request from client to pokemon/name route

--- client/src/components/PokemonDetails.js
+++ client/src/components/PokemonDetails.js
@@ -1,5 +1,14 @@
 import React from "react";
 
-export default function pokemonDetails() {
-  return <div></div>;
+export default function pokemonDetails({ pokemon }) {
+  return (
+    <div>
+      <ul className="ul-pokemon-details">
+        <li>Name: {pokemon.data.name}</li>
+        <li>Height: {pokemon.data.height}</li>
+        <li>Weight: {pokemon.data.weight}</li>
+        <li>Types: {pokemon.data.types}</li>
+      </ul>
+    </div>
+  );
 }
--- client/src/components/SearchArea.js
+++ client/src/components/SearchArea.js
@@ -1,5 +1,32 @@
-import React from "react";
+import { React, useState } from "react";
+import PokemonDetails from "./PokemonDetails";
+import axios from "axios";
+const URL = "http://localhost:3001/api/pokemon/";
 
 export default function SearchArea() {
-  return <div></div>;
+  const [inputValue, setInputValue] = useState("");
+  const [pokemon, setPokemon] = useState("");
+
+  const getPokemonDetails = async () => {
+    const pokemonName = inputValue;
+    setPokemon(await axios.get(`${URL}/${pokemonName}`));
+  };
+
+  const handleInput = (e) => {
+    setInputValue(e.target.value.toLowerCase());
+  };
+
+  return (
+    <div className="search-area">
+      <input
+        className="search-input"
+        placeholder="Search Pokémon"
+        onChange={handleInput}
+      ></input>
+      <button className="search-button" onClick={getPokemonDetails}>
+        Search
+      </button>
+      <PokemonDetails pokemon={pokemon} />
+    </div>
+  );
 }

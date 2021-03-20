8ed995b4f2753182ff87f1ff2a31d3500f48e5e6
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-20 13:21:10 +0200

    added functionality to collection item
    when clicking on name, we get information on the pokemon we clicked

--- client/src/components/CollectionItem.js
+++ client/src/components/CollectionItem.js
@@ -1,5 +1,26 @@
 import React from "react";
+import { useState } from "react";
 
 export default function CollectionItem({ pokemonName }) {
-  return <div className="pokemon-collection-item">{pokemonName.name}</div>;
+  const [bool, setBool] = useState(true);
+  const showData = () => {
+    if (bool) {
+      setBool(false);
+    } else {
+      setBool(true);
+    }
+  };
+  let pokemonTypes = "";
+  pokemonName.types.forEach((type) => {
+    pokemonTypes = `${pokemonTypes}  ${type}`;
+  });
+  return (
+    <div className="pokemon-collection-item" onClick={showData}>
+      {pokemonName.name}
+      <span className="pokemon-data" hidden={bool}>
+        - Id: {pokemonName.id}, Height: {pokemonName.height}, Weight:{" "}
+        {pokemonName.weight}, Types: {pokemonTypes}
+      </span>
+    </div>
+  );
 }

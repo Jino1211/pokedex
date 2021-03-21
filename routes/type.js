13ff4dc504c2c368b3871efcf636aa0834f35ee8
eyalzimerman <77383315+eyalzimerman@users.noreply.github.com>
2021-03-21 09:30:39 +0200

    Update type.js

--- routes/type.js
+++ routes/type.js
@@ -24,26 +24,4 @@ type.get("/:typeName", (req, res) => {
     });
 });
 
-// Trying to get the img types from server. but cannot loading the src at time
-
-// type.get("/:typeName", async (req, res) => {
-//   const { typeName } = req.params;
-//   const pokemons = await axios.get(`${POKEAPI_BASE_URL}/type/${typeName}`);
-//   const pokemonsDataTypes = {
-//     id: pokemons.data.id,
-//     name: pokemons.data.name,
-//     pokemons: [],
-//   };
-//   pokemons.data.pokemon.forEach(async (pokemonName) => {
-//     // console.log(pokemonName.pokemon);
-//     const result = await axios.get(
-//       `${POKEAPI_BASE_URL}/pokemon/${pokemonName.pokemon.name}`
-//     );
-//     pokemonName.pokemon.src = result.data.sprites.front_default;
-//     // console.log(pokemonName.pokemon);
-//     pokemonsDataTypes.pokemons.push(pokemonName.pokemon);
-//     console.log(pokemonsDataTypes);
-//   });
-//   res.send(pokemonsDataTypes);
-// });
 module.exports = type;
